d21a2ea8b7615cc3a6cf0416cc299b0e83a02e6e
Han-Note\huccc <huccce@gmail.com>
2021-03-18 16:04:55 +0900

    누락된 Etc

--- CSVTrans/CSVTrans.py
+++ CSVTrans/CSVTrans.py
@@ -138,7 +138,8 @@ def ConvertTest(loadFile, language, languageFull):
     print(languageFull + ' ' + loadFile)
 
 #불러올 데이터들
-loadList = ['AccountBox', 'Etc', 'MatchCategory', 'MatchItem', 'Notice', 'Player', 'Script', 'ShopItem', 'Tutorial', 'Team', 'Store']
+#loadList = ['AccountBox', 'Etc', 'MatchCategory', 'MatchItem', 'Notice', 'Player', 'Script', 'ShopItem', 'Tutorial', 'Team', 'Store']
+loadList = ['Etc']
 #replaceList = ['AccountBox', 'Etc', 'MatchCategory', 'MatchItem', 'Notice', 'Script', 'ShopItem', 'Tutorial']
 notReplaceList = ['Team', 'Player']
 #일본어, 중국어간체, 중국어번체, 베트남어, 독일어, 러시아어, 스페인어, 아랍어, 이탈리아어, 말레이어, 태국어, 터키어, 프랑스어, 인도네시아어, 자바어, 뱅골어, 힌디어, 포르투칼어
